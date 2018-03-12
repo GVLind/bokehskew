cb1bdc1db5eab81b2b4637ad06b386feabf3118a
GVLind <vctrlndh@gmail.com>
2018-03-12 20:00:56 +0100

    adds coloured GC scoring to graph

--- src/usecolumnDataframe.py
+++ src/usecolumnDataframe.py
@@ -2,7 +2,7 @@
 
 from Bio import SeqIO, SeqUtils
 import pandas as pd
-from bokeh.models import ColumnDataSource, Select
+from bokeh.models import ColumnDataSource, Select, LinearAxis, Range1d
 from bokeh.plotting import figure
 from bokeh.io import curdoc, show, output_file
 from bokeh.layouts import layout
@@ -15,27 +15,31 @@ import random
 
 def calculateSkew(fullPath):
 
-	df = pd.DataFrame(columns=["id","x","y"])
+	df = pd.DataFrame(columns=["id","x","y","z"])
 	df_index = 0
 
 	with open (fullPath, "r") as handle:
 	    
 		for record in SeqIO.parse(handle, "fasta"):
 
-			skew = [0]
-			gc = SeqUtils.GC_skew(record.seq, 1000)
-			for nc in gc:
-				skew.append(skew[-1]+nc)
-			skew = skew[1:]
+			gc = SeqUtils.GC_skew(record.seq, 10000)
 
-			for i in enumerate(skew):
-				df.loc[df_index] = [record.id, i[0], i[1]]
-				df_index += 1
+			count = 0
+			skew = 0
+			for nc in gc:
+
+				count += 1 
+				skew = skew + nc
+				df.loc[df_index] = [record.id, count, skew, nc ]
+				df.index += 1
+
 
 	return df
 
 def randomColor():
 	randomColor = random.sample(range(1, 220), 3)
+
+
 	return tuple(randomColor)
 
 def plotBokeh(source):
@@ -46,12 +50,19 @@ def plotBokeh(source):
 
 	'''
 
-	plot = figure()	
+	plot = figure()
+
+	plot.extra_y_ranges = {"GC": Range1d(start=-0.1, end=0.1)}
+
+	plot.add_layout(LinearAxis(y_range_name="GC"), "right")
+
 	for uniqID in source.id.unique():
 		ID = ColumnDataSource(source[source["id"]==uniqID])
 
+		col = randomColor()
 		plot.title.text = strain
-		plot.line(x = "x", y = "y", source = ID, color = randomColor())
+		plot.line(x = "x", y = "y", source = ID, color = col)
+		plot.line(x = "x", y= "z", source = ID, color = col, line_alpha = .15, y_range_name = "GC")
 	return plot
 
 def plotUpdate(attrname, old, new):
@@ -90,14 +101,16 @@ strain_select = Select(value = strain, title = "Strain", options = sorted(strain
 
 source = calculateSkew(strains[strain]["path"])
 
+print (source)
+
 plot = plotBokeh(source)
 
-#output_file=("line.html")
+output_file=("../html/line.html")
 
 strain_select.on_change("value", plotUpdate)
 
-#show(plot)
+show(plot)
 controls = layout([strain_select])
 
-curdoc().add_root(controls)
-curdoc().add_root(plot)
+#curdoc().add_root(controls)
+#curdoc().add_root(plot)
